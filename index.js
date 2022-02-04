16c76d03e731e3144abb6cc620a5dc600f12e1ff
Onki Eceron <pratamaonki@gmail.com>
2022-02-04 17:45:25 +0700

    first one

---
+++ index.js
@@ -0,0 +1,55 @@
+/*// alert("hello world");
+// document.getElementById("hello_word").innerHTML = "My Name Is Onki Pratama";
+// console.log("My Name Is Onki Pratama");
+
+//string
+var myName = "My Name Is Onki Pratama";
+var address = "South Bengkulu";
+var my_name = "Jaka";
+var MY_NAME = "Jiki";
+//integer
+var firstNumber = 1;
+var secondNumber = 2;
+// decimal
+var someDecimal = 1.4;
+// boolean
+var isHandsome = true;
+var isUgly = false;
+
+// array
+var fruit = ["Apple", "Mangoes", "Watermelon"];
+
+// object
+var motor = {
+  name: "Supra X",
+  color: "Red-Black",
+  cc: 600,
+};
+
+// console.log(myName);
+// console.log(address);
+// console.log(firstNumber);
+// console.log(secondNumber);
+// console.log(someDecimal);
+// console.log(isHandsome);
+// console.log(fruit);
+// console.log(motor);
+// console.log(motor.name);
+
+var one = 1;
+var two = 2;
+// console.log(one + 5);
+two += 1;
+// console.log(two);
+
+var firstName = "Onki";
+var lastName = "Pratama";
+// console.log(firstName + " " + lastName);
+console.log(firstName + one + two);
+console.log(firstName + (one + two));*/
+
+// function
+function add() {
+  console.log(1 + 2);
+}
+add();
---
+++ index2.js
@@ -0,0 +1,6 @@
+function add() {
+  var one = 1;
+  var two = 2;
+  return one + two;
+}
+console.log(add());
